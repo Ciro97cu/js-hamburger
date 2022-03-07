e9e96f9ca76dc99ebd49552af2251779b742c342
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-07 14:22:12 +0100

    added event on hamburger menu

---
+++ main.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector(".fa-bars");
+hamburger.addEventListener("click", function () {
+    let menuOn = document.querySelector(".hamburger-menu");
+    menuOn.style.display = "block";
+});
