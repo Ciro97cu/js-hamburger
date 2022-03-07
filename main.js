a0c14c1a258f7d7b8c16d0c1716f2c4a3571d245
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-07 17:43:45 +0100

    added a short version

--- main.js
+++ main.js
@@ -14,3 +14,14 @@ back.addEventListener("click", function () {
     // class display block
     menuOnOff.classList.toggle("active");
 });
+
+/* short version
+
+document.querySelector(".fa-bars").addEventListener("click", function () {
+    document.querySelector(".hamburger-menu").classList.toggle("active");
+});
+
+document.querySelector(".fa-times").addEventListener("click", function () {
+    document.querySelector(".hamburger-menu").classList.toggle("active");
+}); 
+*/
