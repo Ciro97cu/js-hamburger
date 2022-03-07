af92eebd956ed47a25dcd52ba7940f1d21ba5e21
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-07 14:57:33 +0100

    added comments

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+// declared the hamburger variable corresponding to the tag "i" fa-bars
 const hamburger = document.querySelector(".fa-bars");
+// added a click event to the hamburger variable
 hamburger.addEventListener("click", function () {
+    // declared the variable corresponding to the hambuger menu to be displayed
     let menuOn = document.querySelector(".hamburger-menu");
+    // display block
     menuOn.style.display = "block";
 });
-
+// declared the back variable corresponding to the tag "i" fa-times
 const back = document.querySelector(".fa-times");
+// added a click event to the back variable
 back.addEventListener("click", function () {
+    // declared the variable corresponding to the hambuger menu to be made to disappear
     let menuOff = document.querySelector(".hamburger-menu");
+    // display none
     menuOff.style.display = "none";
 });
